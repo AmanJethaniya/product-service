4cb5f5ef24200b84f604a5924368c804d6d9d365
Aman Jethaniya <amanjethaniya619@gmail.com>
2023-01-08 23:59:20 +0530

    Added test or create and get end point.

--- src/test/java/com/onlineshop/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/onlineshop/productservice/ProductServiceApplicationTests.java
@@ -4,21 +4,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.onlineshop.productservice.model.ProductRequest;
 import com.onlineshop.productservice.repository.ProductRepository;
 
+import junit.framework.Assert;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.client.RestTemplate;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-
+import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,9 +34,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Testcontainers
 @AutoConfigureMockMvc
 class ProductServiceApplicationTests {
+	 private static Logger LOGGER = LoggerFactory.getLogger(ProductServiceApplicationTests.class);
+
 
     //@Container
     static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+    @Autowired
+    private RestTemplate restTemplate;
     @Autowired
     private MockMvc mockMvc;
     @Autowired
@@ -51,7 +63,12 @@ class ProductServiceApplicationTests {
                 .andExpect(status().isCreated());
         Assertions.assertEquals(1, productRepository.findAll().size());
     }
+    @Test
     void testGetEndPointViaRestTemplate() {
+    	LOGGER.info("Hitting get URL: http://localhost:8080/api/product");
+    	ResponseEntity<ArrayList> response = restTemplate.getForEntity("http://localhost:8080/api/product", ArrayList.class);
+    	LOGGER.info("Received response with status {}", response.getStatusCode());
+    	Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
     	
     }
 
